6f0f1e95315e9b387e471e82431e9c19a845469b
Ian de Jesus <ian.dejesus.md@gmail.com>
2016-12-04 09:15:11 +0800

    Put an object in Parse

--- app/src/main/java/io/techup/android/instagramclone/MainActivity.java
+++ app/src/main/java/io/techup/android/instagramclone/MainActivity.java
@@ -2,8 +2,12 @@ package io.techup.android.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+        ParseObject score = new ParseObject("Score");
+        score.put("username", "rob");
+        score.put("score", 86);
+        score.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    Log.i("SaveInBackground", "Success");
+                }else{
+                    Log.i("SaveInBackground", "Failed Error:" + String.valueOf(e));
+
+                }
+            }
+        });
 
     }
 }
