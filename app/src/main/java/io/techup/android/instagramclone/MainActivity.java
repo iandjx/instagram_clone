459192f183496b00a378841b0d8ba7cd614a6577
Ian de Jesus <ian.dejesus.md@gmail.com>
2016-12-04 08:13:35 +0800

    Add boilerplate code for connecting to Parse server

--- app/src/main/java/io/techup/android/instagramclone/MainActivity.java
+++ app/src/main/java/io/techup/android/instagramclone/MainActivity.java
@@ -3,11 +3,16 @@ package io.techup.android.instagramclone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.parse.ParseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
     }
 }
---
+++ app/src/main/java/io/techup/android/instagramclone/StarterApplication.java
@@ -0,0 +1,55 @@
+package io.techup.android.instagramclone;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+public class StarterApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("2ba598bec6e9f58338dc9d3f9781a4c2a420a62e")
+                .clientKey("810540d965b756b365b192c61e9ce2a5aaef68d9")
+                .server("http://ec2-35-164-225-183.us-west-2.compute.amazonaws.com:80/parse/")
+                .build()
+        );
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("myNumber", "123");
+        object.put("myString", "rob");
+
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("Parse Result", "Successful!");
+                } else {
+                    Log.i("Parse Result", "Failed" + ex.toString());
+                }
+            }
+        });
+
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+
+}
