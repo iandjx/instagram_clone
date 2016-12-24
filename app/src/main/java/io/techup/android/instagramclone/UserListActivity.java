4953737340d078d295dc5cabcb76e27f0a8bb655
Ian de Jesus <ian.dejesus.md@gmail.com>
2016-12-24 13:53:41 +0900

    Query Parse for list of users and display in listview

--- app/src/main/java/io/techup/android/instagramclone/UserListActivity.java
+++ app/src/main/java/io/techup/android/instagramclone/UserListActivity.java
@@ -5,29 +5,52 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UserListActivity extends AppCompatActivity {
 
-    ArrayList<ParseUser> parseUsers;
-    ListView userListView;
     ArrayList<String> usernames;
-
+    ListView userListView;
+    ParseQuery<ParseUser> query;
+    ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
 
-        userListView = (ListView) findViewById(R.id.lv_userList);
         usernames = new ArrayList<>();
-        usernames.add("ian");
-        usernames.add("ian2");
-        usernames.add("ian3");
-        usernames.add("ian4");
+        userListView = (ListView) findViewById(R.id.lv_userList);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, usernames);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, usernames);
-        userListView.setAdapter(arrayAdapter);
+        query = ParseUser.getQuery();
+        query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        query.addAscendingOrder("username");
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e== null){
+                    if(objects.size()>0){
+                        for(ParseUser user: objects){
+                            usernames.add(user.getUsername());
+                        }
+                        userListView.setAdapter(arrayAdapter);
+
+                    }
+
+                }else{
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
     }
 }
