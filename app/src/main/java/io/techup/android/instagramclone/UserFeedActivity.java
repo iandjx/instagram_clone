61d07fad7abc1c1fcbbfb64e6da094aac80e35fa
Ian de Jesus <ian.dejesus.md@gmail.com>
2016-12-25 17:19:25 +0900

    Display image/s in UserFeedActivity of selected user

--- app/src/main/java/io/techup/android/instagramclone/UserFeedActivity.java
+++ app/src/main/java/io/techup/android/instagramclone/UserFeedActivity.java
@@ -1,10 +1,22 @@
 package io.techup.android.instagramclone;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class UserFeedActivity extends AppCompatActivity {
     LinearLayout linearLayout;
@@ -13,15 +25,49 @@ public class UserFeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_feed);
+
+        Intent intent = getIntent();
+
+        String activeUsername = intent.getStringExtra("username");
+
+        setTitle(activeUsername + "'s Feed ");
+
+        ParseQuery<ParseObject> parseQuery = new ParseQuery<>("Image");
+        parseQuery.whereEqualTo("username", activeUsername);
+        parseQuery.orderByDescending("createdAt");
+
+        parseQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    if (objects.size() > 0) {
+                        for (ParseObject parseObject : objects) {
+                            ParseFile parseFile = (ParseFile) parseObject.get("image");
+                            parseFile.getDataInBackground(new GetDataCallback() {
+                                @Override
+                                public void done(byte[] data, ParseException e) {
+                                    if (e == null && data != null) {
+                                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                        ImageView imageView = new ImageView(getApplicationContext());
+
+                                        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                                ViewGroup.LayoutParams.WRAP_CONTENT
+                                        ));
+                                        imageView.setImageBitmap(bitmap);
+
+                                        linearLayout.addView(imageView);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        });
+
+
         linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
-        ImageView imageView = new ImageView(getApplicationContext());
 
-        imageView.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        ));
-        imageView.setImageDrawable(getResources().getDrawable(R.drawable.pencil));
-
-        linearLayout.addView(imageView);
     }
 }
